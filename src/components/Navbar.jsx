dd83ea4857e38906c1f6c03aae7293ce730d7d36
akshay7745 <akshay7745@gmail.com>
2025-11-23 09:46:49 +0530

    add header footer and routing in the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-200 shadow-sm px-11">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">
+          Dev Connections+
+        </Link>
+      </div>
+      <div className="flex gap-2">
+        <div>
+          <Link className="btn btn-ghost text-xl" to="/dashboard">
+            Dashboard
+          </Link>
+        </div>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <Link to="/profile" className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
